39b4fd6092cf3dbbbaa42190a2850a0cd7c8ebcb
Matias G <matigodoy93@hotmail.com>
2024-11-11 21:20:49 -0300

    proyectos

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -79,54 +79,6 @@ export const Projects = () => {
               </div>
             </Link>
         </div>
-        
-        <div className="mb-4 lg:w-2/4 lg:mb-14">
-          <h4 className="text-3xl text-center leading-6 mb-6 md:text-4xl">Cine Online</h4>
-            {/* Títulos */}
-            <Link to="/project/6">
-              <div className="w-full h-60 md:h-72 flex flex-col items-center md:w-3/4 md:mx-auto lg:w-full lg:mb-12">
-                <div className=" project-glow project bg-blue-900 transition-all ease-in-out w-full duration-300 lg:w-3/4 rounded-xl mb-10 lg:mb-0 lg:relative hover:-translate-y-1 lg:opacity-75 lg:hover:opacity-100">
-                  <img className='h-72 w-full' src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Aspect-ratio-16x9.svg/1200px-Aspect-ratio-16x9.svg.png" alt="Certificado de aprobación en Educación IT" />
-                </div>
-              </div>
-            </Link>
-        </div>
-        
-        <div className="mb-4 lg:w-2/4 lg:mb-14">
-          <h4 className="text-3xl text-center leading-6 mb-6 md:text-4xl">Cine Online</h4>
-            {/* Títulos */}
-            <Link to="/project/7">
-              <div className="w-full h-60 md:h-72 flex flex-col items-center md:w-3/4 md:mx-auto lg:w-full lg:mb-12">
-                <div className=" project-glow project bg-blue-900 transition-all ease-in-out w-full duration-300 lg:w-3/4 rounded-xl mb-10 lg:mb-0 lg:relative hover:-translate-y-1 lg:opacity-75 lg:hover:opacity-100">
-                  <img className='h-72 w-full' src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Aspect-ratio-16x9.svg/1200px-Aspect-ratio-16x9.svg.png" alt="Certificado de aprobación en Educación IT" />
-                </div>
-              </div>
-            </Link>
-        </div>
-        
-        <div className="mb-4 lg:w-2/4 lg:mb-14">
-          <h4 className="text-3xl text-center leading-6 mb-6 md:text-4xl">Cine Online</h4>
-            {/* Títulos */}
-            <Link to="/project/8">
-              <div className="w-full h-60 md:h-72 flex flex-col items-center md:w-3/4 md:mx-auto lg:w-full lg:mb-12">
-                <div className=" project-glow project bg-blue-900 transition-all ease-in-out w-full duration-300 lg:w-3/4 rounded-xl mb-10 lg:mb-0 lg:relative hover:-translate-y-1 lg:opacity-75 lg:hover:opacity-100">
-                  <img className='h-72 w-full' src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Aspect-ratio-16x9.svg/1200px-Aspect-ratio-16x9.svg.png" alt="Certificado de aprobación en Educación IT" />
-                </div>
-              </div>
-            </Link>
-        </div>
-        
-        <div className="mb-4 lg:w-2/4 lg:mb-14">
-          <h4 className="text-3xl text-center leading-6 mb-6 md:text-4xl">Cine Online</h4>
-            {/* Títulos */}
-            <Link to="/project/9">
-              <div className="w-full h-60 md:h-72 flex flex-col items-center md:w-3/4 md:mx-auto lg:w-full lg:mb-12">
-                <div className=" project-glow project bg-blue-900 transition-all ease-in-out w-full duration-300 lg:w-3/4 rounded-xl mb-10 lg:mb-0 lg:relative hover:-translate-y-1 lg:opacity-75 lg:hover:opacity-100">
-                  <img className='h-72 w-full' src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Aspect-ratio-16x9.svg/1200px-Aspect-ratio-16x9.svg.png" alt="Certificado de aprobación en Educación IT" />
-                </div>
-              </div>
-            </Link>
-        </div>
 
       </div>
       
